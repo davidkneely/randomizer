3150bacb9b648d9b138992cd2362492bd6da3546
David N <deeniri@gmail.com>
2015-02-11 11:51:18 -1000

    Updated java.util.Date import. Code compiles fine but does not run due to error: "Exception in thread "main" java.lang.NoSuchMethodException: Randomizer" still does not compile. Removed the surrounding Test code and now the main method is visible. Compiles and runs now.

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,5 +1,4 @@
 /*
- *
  * This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
  * Copyright (c) 1997 by David Flanagan
  * This example is provided WITHOUT ANY WARRANTY either expressed or implied.
@@ -7,13 +6,15 @@
  * For any commercial use, see http://www.davidflanagan.com/javaexamples
  */
 
+import java.util.Date;
+
 /**
  * This class defines methods for computing pseudo-random numbers, and it defines
  * the state variable that needs to be maintained for use by those methods.
  */
 public class Randomizer {
-  static final int MODULUS_CONSTANT = 233280;  // the "modulus"
-  static final int MULTIPLIER_CONSTANT = 9301;    // the "multiplier"
+  static final int MODULUS_CONSTANT = 233280;
+  static final int MULTIPLIER_CONSTANT = 9301;
   static final int INCREMENT_CONSTANT = 49297;
 
   // The state variable maintained by each Randomizer instance
@@ -43,26 +44,25 @@ public class Randomizer {
    * This method computes a pseudo-random integer between 0 and specified
    * maximum.  It uses randomFloat() above.
    * @param max The maximum input.
-   * @return The rounded float.
+   * @return The rounded int.
    */
   public int randomInt(int max) {
       return Math.round(max * randomFloat());
   }
 
+
   /**
-   * This nested class is a simple test program: it prints 10 random integers.
-   * Note how the Randomizer object is seeded using the current time.
+   * Prints 10 random floats between the ranges 0 and 100.
+   * @param args The commandline arguments.
    */
-  public static class Test {
-    /**
-     * Prints 10 random floats between the ranges 0 and 100.
-     * @param args The commandline arguments.
-     */
-      public static void main(String[] args) {
-        Randomizer r = new Randomizer(new java.util.Date().getTime());
-        for (int i = 0; i < 10; i++) {
-          System.out.println(r.randomInt(100));
-        }
-      }
+  public static void main(String[] args) {
+
+    Date date = new java.util.Date();
+    long inputSeed = (long) date.getTime();
+    Randomizer randomizer = new Randomizer(inputSeed);
+    for (int i = 0; i < 10; i++) {
+      System.out.println(randomizer.randomInt(100));
+    }
   }
+
 }
